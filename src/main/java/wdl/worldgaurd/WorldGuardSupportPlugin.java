51476d3dea90e9a7c02e7f5238fe14f7a67e9aee
Pokechu22 <Pokechu022@gmail.com>
2016-03-19 21:08:31 -0700

    Add MCStats support

--- src/main/java/wdl/worldgaurd/WorldGuardSupportPlugin.java
+++ src/main/java/wdl/worldgaurd/WorldGuardSupportPlugin.java
@@ -3,13 +3,54 @@ package wdl.worldgaurd;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
+import org.mcstats.Metrics.Graph;
+import org.mcstats.Metrics.Plotter;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 import wdl.RangeGroupTypeRegistrationEvent;
+import wdl.range.IRangeProducer;
 
 public class WorldGuardSupportPlugin extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		try {
+			class StringPlotter extends Plotter {
+				public StringPlotter(String str) {
+					super(str);
+				}
+				
+				@Override
+				public int getValue() {
+					return 1;
+				}
+			}
+			
+			Metrics metrics = new Metrics(this);
+			
+			Graph worldGuardVersionGraph = metrics.createGraph("worldGuardVersion");
+			String worldGuardVersion = getProvidingPlugin(WorldGuardPlugin.class)
+					.getDescription().getFullName();
+			worldGuardVersionGraph.addPlotter(new StringPlotter(worldGuardVersion));
+			
+			Graph worldEditVersionGraph = metrics.createGraph("worldEditVersion");
+			String worldEditVersion = getProvidingPlugin(WorldEditPlugin.class)
+					.getDescription().getFullName();
+			worldEditVersionGraph.addPlotter(new StringPlotter(worldEditVersion));
+			
+			Graph wdlcVersionGraph = metrics.createGraph("wdlcompanionVersion");
+			String wdlcVersion = getProvidingPlugin(IRangeProducer.class)
+					.getDescription().getFullName();
+			wdlcVersionGraph.addPlotter(new StringPlotter(wdlcVersion));
+			
+			metrics.start();
+		} catch (Exception e) {
+			getLogger().warning("Failed to start PluginMetrics :(");
+		}
 	}
 	
 	@EventHandler
